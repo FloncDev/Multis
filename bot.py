0a5bca7902cca3f591031daa272b2919a870f2ae
DevFlock <ytman98@gmail.com>
2021-05-09 22:31:56 +0100

    Check last commit for changes

--- bot.py
+++ bot.py
@@ -47,6 +47,6 @@ async def reload(ctx):
     await ctx.message.add_reaction("✅")
 
 try:
-    client.run("NzkyODAxODIyNTk0MzAxOTUy.X-jAPA.ocO48r_p7YsojfUIg3OVONVxbsE") # Put your own token here (Don't share it!)
+    client.run("") # Put your own token here (Don't share it!)
 except ClientException:
     print("Token is invalid. Please enter a new one")
